4ba4c4ea34326fd767bd267cbc9ecae94af61130
Ohkubo KOHEI <ohkubo@heartrails.com>
2023-08-12 09:14:10 +0900

    add og tags

--- main.ts
+++ main.ts
@@ -17,8 +17,14 @@ app.get("/typistan", (c) => {
     image: `/typistan.png?title=${title}&score=${score}`,
   }
   const href = `https://typing.kbn.one/#${csv || ""}`
+  const og_url = `https://kuboon-ogp.deno.dev/typistan?title=${title}&score=${score}&csv=${csv}`
   const metas = Object.entries(og).map(([k, v]) => (
-    html`<meta property='og:${k}' content='${v}' />`
+    html`
+    <meta property='og:${k}' content='${v}' />
+    <meta property='og:url' content='${og_url}' />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="たいぴすたん ${title} で ${score}てん" />
+    `
   ));
   return c.html(html`
     <html>
