7b05b1dadab02983d77d21c1663d522cd51b2355
Ohkubo KOHEI <ohkubo@heartrails.com>
2023-08-16 11:06:02 +0900

    return 404 unless score

--- main.ts
+++ main.ts
@@ -12,24 +12,28 @@ type Query = {
 
 app.get("/typistan", (c) => {
   const base = new URL(c.req.url).origin
-  const {score, title, csv} = c.req.query() as Query;
+  const { score, title: title_, csv } = c.req.query() as Query;
+  if (!score || score === "0") return c.notFound();
+
+  const title = (!title_ || title_ == '') ? null : title_;
   const og = {
-    title: `たいぴすたん ${title} で ${score} てん`,
-    image: `${base}/typistan.png?title=${title}&score=${score}`,
+    title: `たいぴすたん${title ? ` ${title} ` : ''}で ${score} てん`,
+    image: `${base}/typistan.png?score=${score}` + (title ? `&title=${title}` : ''),
     'image:type': 'image/png',
-    url: `${base}/typistan?title=${title}&score=${score}&csv=${csv}`,
+    url: `${base}/typistan?score=${score}&title=${title}&&csv=${csv}`,
     type: "website",
-    description: `たいぴすたん ${title} で ${score}てん`,
+    description: `たいぴすたん で たいぴんぐ ちゃれんじ！`,
   }
   const metas = Object.entries(og).map(([k, v]) => (
-    html`
-    <meta property='og:${k}' content='${v}' />
+    html`<meta property='og:${k}' content='${v}' />
     `
   ));
   const href = `https://typing.kbn.one/#${csv || ""}`
   return c.html(html`
     <html>
       <head>
+        <meta charset='utf-8' />
+        <title>${og.title}</title>
         ${metas}
       </head>
       <body style='width: 100svw; height: 100svh; '>
@@ -42,7 +46,7 @@ app.get("/typistan", (c) => {
   );
 });
 app.get("/typistan.png", (c) => {
-  const {score, title} = c.req.query() as Query;
+  const { score, title } = c.req.query() as Query;
   return typistanImg({ score, title });
 });
 
